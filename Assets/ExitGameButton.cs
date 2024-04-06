1c883a7e2800d34da92a9d509a5b6c04e10a752a
PranavReddy0148 <pranav.katreddy0148@gmail.com>
2024-04-07 05:09:32 +0530

    Some changes

--- Assets/ExitGameButton.cs
+++ Assets/ExitGameButton.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGameButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ExitMiniGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SC Pixel Art Top Down - Basic");
     }
 }
